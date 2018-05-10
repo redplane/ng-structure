52f2199791e168137d3a0519fbfbdf05a8e7fa9c
Linh Nguyen <redplane_dt@yahoo.com.vn>
2018-05-10 15:08:29 +0800

    - Added travis CI. - Added plugin option - Added bitballoon deployment.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,10 @@
 module.exports = (env, argv) => {
-    let path = require('path');
-    let webpack = require('webpack');
-    let CleanWebpackPlugin = require('clean-webpack-plugin');
-    let CopyWebpackPlugin = require('copy-webpack-plugin');
-    let HtmlWebpackPlugin = require('html-webpack-plugin');
-    let ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
-    let BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+    const path = require('path');
+    const moduleRuleOption = require('./webpack/rule.option');
+    const modulePluginOption = require('./webpack/plugin.option');
 
     // Import webpack settings.
-    let settings = require('./webpack/webpack-setting');
-    let options = {
-        clean: require('./webpack/clean-webpack.setting').get(__dirname),
-        copy: require('./webpack/copy-webpack.setting').get(__dirname)
-    };
+    const webpackOption = require('./webpack/option');
 
     // True if built is set to production mode.
     // False if built is set to development mode.
@@ -20,82 +12,17 @@ module.exports = (env, argv) => {
 
     // Build path options.
     let paths = {
-        source: settings.paths.getSource(__dirname),
-        app: settings.paths.getApplication(__dirname),
-        dist: settings.paths.getDist(__dirname)
+        root: __dirname,
+        source: webpackOption.paths.getSource(__dirname),
+        app: webpackOption.paths.getApplication(__dirname),
+        dist: webpackOption.paths.getDist(__dirname)
     };
-
-    /*
-    * Plugins import.
-    * */
-    let plugins = [];
-
-    // Remove obsoleted chunks.
-    let CleanObsoleteChunks = require('webpack-clean-obsolete-chunks');
-    plugins.push(new CleanObsoleteChunks({verbose: true}));
-
-    // Clean fields before publishing packages.
-    plugins.push(new CleanWebpackPlugin(options.clean.paths, options.clean.options));
-
-    /*
-    * Enlist plugins which should be run on production mode.
-    * */
-    if (bProductionMode) {
-        //Automatically add annotation to angularjs modules.
-        // Bundling can affect module initialization.
-        plugins.push(new ngAnnotatePlugin({add: true}));
-    }
-
-    /*
-    * Not in production mode
-    * */
-    if (!bProductionMode) {
-        // Require original index file.
-        let browserSyncPlugin = new BrowserSyncPlugin({
-            // browse to http://localhost:3000/ during development,
-            // ./public directory is being served
-            host: 'localhost',
-            port: 8000,
-            files: [
-                path.resolve(paths.source, 'index.html')
-            ],
-            server: {
-                baseDir: [
-                    paths.dist
-                ]
-            }
-        });
-
-        // Push plugins into list.
-        plugins.push(browserSyncPlugin);
-    }
-
-    /*
-    * Enlist default plugins.
-    * */
-    // Copy files.
-    plugins.push(new CopyWebpackPlugin(options.copy));
-
-    // Using bluebird promise instead of native promise.
-    plugins.push(new webpack.ProvidePlugin({
-        Promise: 'bluebird',
-        moment: 'moment'
-    }));
-
-
-    //Automatically inject chunks into html files.
-    plugins.push(new HtmlWebpackPlugin({
-        template: path.resolve(paths.source, 'index.html'),
-        chunksSortMode: 'dependency'
-    }));
-
-    let moduleRuleOption = require('./webpack/rule-webpack.setting');
 
     /*
     * Module export.
     * */
     return {
-        context: settings.paths.getSource(__dirname),
+        context: webpackOption.paths.getSource(__dirname),
         entry: {
             'jQueryVendors': ['jquery', 'bluebird', 'bootstrap', 'admin-lte', 'moment'],
             'angularVendors': [
@@ -125,7 +52,7 @@ module.exports = (env, argv) => {
         module: {
             rules: moduleRuleOption.get()
         },
-        plugins: plugins,
+        plugins: modulePluginOption.get(bProductionMode, paths),
         output: {
             path: path.resolve(paths.dist),
             filename: '[name].[hash].js'
